f502e7b32b2f3462b109855a68ee33968f90e8f3
jaewoo <comjayoncloud@gmail.com>
2022-05-11 12:41:34 +0900

    2022.05.11 ver1

--- lolproject-backend/app.js
+++ lolproject-backend/app.js
@@ -3,53 +3,61 @@ const app = express();
 const port = 3000;
 const request = require("request");
 const axios = require("axios");
+const { get } = require("request");
 
 https: app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send();
+app.get("/", async (req, res) => {
+  const summoner = await getSummoner(
+    "%EB%B9%B5%EB%92%A4%EB%A5%BC%ED%9D%94%EB%93%9C%EB%A1%9D%EB%B0%94"
+  );
+  const matchIdList = await getMatchId(
+    "r8N_A7VuWeYuN47DEt-R7cSj4OhNMixvqUtxUbpVKXu16fqHlf839AYHiull9_peace0C4eouLo8Yg/ids?start=0&count=20"
+  );
+  const match = await getMatch("KR_5910174625");
 });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
-// api - axios ver
-axios({
-  url: "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EB%B9%B5%EB%92%A4%EB%A5%BC%ED%9D%94%EB%93%9C%EB%A1%9D%EB%B0%94", // 통신할 웹문서
-  method: "get", // 통신할 방식
-  headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-    "Accept-Language": "ko-KR,ko;q=0.9,en-GB;q=0.8,en;q=0.7",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    Origin: "https://developer.riotgames.com",
-    "X-Riot-Token": "put ur token ",
-  },
-  responseType: "json",
-}).then(function (res) {
-  console.log(res.headers);
-  console.log(res.data);
-});
+getSummoner = async (name) => {
+  // api - axios ver
+  console.log("hi");
+  const summoner = await axios.get(
+    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`,
+    {
+      headers: {
+        "X-Riot-Token": "put ur token",
+      },
+    }
+  );
+  console.log(summoner.data);
+  return summoner.data;
+};
 
-// api - request ver
-// const options = {
-//   uri: "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EB%B9%B5%EB%92%A4%EB%A5%BC%ED%9D%94%EB%93%9C%EB%A1%9D%EB%B0%94",
-//   method: "GET",
-//   json: true,
-//   headers: {
-//     "User-Agent":
-//       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-//     "Accept-Language": "ko-KR,ko;q=0.9,en-GB;q=0.8,en;q=0.7",
-//     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-//     Origin: "https://developer.riotgames.com",
-//     "X-Riot-Token": " put ur token ",
-//   },
-// };
+getMatchId = async (puuid) => {
+  const matchId = await axios.get(
+    `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}`,
+    {
+      headers: {
+        "X-Riot-Token": "put ur token",
+      },
+    }
+  );
+  console.log(matchId.data);
+  return matchId.data;
+};
 
-// request.get(options, function (req, res) {
-//   console.log(res.headers);
-//   console.log(res.body);
-//   headers = res.headers;
-//   body = res.body;
-// });
+getMatch = async (s) => {
+  const matchInfo = await axios.get(
+    `https://asia.api.riotgames.com/lol/match/v5/matches/${s}`,
+    {
+      headers: {
+        "X-Riot-Token": "put ur token",
+      },
+    }
+  );
+  console.log(matchInfo);
+  return matchInfo;
+};
