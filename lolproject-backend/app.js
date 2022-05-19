19eb17e3a1780c45ba91915b96421a415f69589b
jaewoo <comjayoncloud@gmail.com>
2022-05-19 23:42:51 +0900

    backend and frontend code changed

--- lolproject-backend/app.js
+++ lolproject-backend/app.js
@@ -11,26 +11,23 @@ app.use(express.json());
 
 app.get("/api/allinfo", async (req, res) => {
   console.log("connected");
+  // puuid 받아오기
   const summoner = await getSummoner(
     "%EB%B9%B5%EB%92%A4%EB%A5%BC%ED%9D%94%EB%93%9C%EB%A1%9D%EB%B0%94" // name
   );
-  console.log(summoner);
+
+  //
   const matchIdList = await getMatchId(
-    "r8N_A7VuWeYuN47DEt-R7cSj4OhNMixvqUtxUbpVKXu16fqHlf839AYHiull9_peace0C4eouLo8Yg/ids?start=0&count=20" //puuid
+    `${summoner.puuid}/ids?start=0&count=20` //puuid
   );
-  console.log(matchIdList);
+
   const matchList = [];
 
   for (const matchId of matchIdList) {
     try {
-      console.log(matchId);
-      matchList.push(await getMatch(matchId));
-    } catch (e) {
-      console.error(e);
-    }
+      matchList.push(await getMatch(matchId, summoner));
+    } catch (e) {}
   }
-
-  console.log(matchList);
 
   res.json(matchList);
 });
@@ -43,17 +40,17 @@ getSummoner = async (name) => {
   // api - axios ver
 
   const url = `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`;
-  // console.log
 
+  // puuid 가져오기
   const summoner = await axios.get(url, {
     headers: {
       "X-Riot-Token": "RGAPI-864d61be-5d5e-41e1-a1b4-497631285277",
     },
   });
-  // console.log(summoner.data);
   return summoner.data;
 };
 
+//matchid 받아오기 ["1232141","123141421"]
 getMatchId = async (puuid) => {
   const matchId = await axios.get(
     `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}`,
@@ -63,11 +60,12 @@ getMatchId = async (puuid) => {
       },
     }
   );
-  // console.log(matchId.data);
+
   return matchId.data;
 };
 
-getMatch = async (s) => {
+// 게임 내용정보 얻어와서 json 형태로 내가 맘에들게하는중
+getMatch = async (s, summoner) => {
   const matchInfo = await axios.get(
     `https://asia.api.riotgames.com/lol/match/v5/matches/${s}`,
     {
@@ -76,65 +74,41 @@ getMatch = async (s) => {
       },
     }
   );
+  const participants = matchInfo.data.info.participants;
+
+  let championName = null;
+  let win = null;
+  let stat = null;
+  let myteamlist = [];
+  let notmyteamlist = [];
+
+  participants.forEach((x, index) => {
+    if (x.puuid == summoner.puuid) {
+      championName = x.championName;
+      win = x.win ? "승" : "패";
+      stat = x.kills + "/" + x.deaths + "/" + x.assists;
+    }
+  });
+
+  participants.forEach((x, index) => {
+    if (x.teamId == "100") {
+      myteamlist.push({ champ: x.championName, name: x.summonerName });
+    } else if (x.teamId == "200") {
+      notmyteamlist.push({ champ: x.championName, name: x.summonerName });
+    }
+  });
 
   if (matchInfo.data.info.gameMode == "ARAM") {
     matchInfo.data.info.gameMode = "무작위총력전";
   }
-  if (matchInfo.data.info.participants[9].win == false) {
-    matchInfo.data.info.participants[9].win = "패";
-  } else matchInfo.data.info.participants[9].win = "승";
 
   let allInfo = {
-    gameType: matchInfo.data.info.gameMode,
-    gameResult: matchInfo.data.info.participants[9].win,
-    champName: matchInfo.data.info.participants[9].championName,
-    gameStat: `${matchInfo.data.info.participants[9].kills}/${matchInfo.data.info.participants[9].deaths}/${matchInfo.data.info.participants[9].assists}`,
-    myTeam: [
-      {
-        champ: matchInfo.data.info.participants[9].championName,
-        name: matchInfo.data.info.participants[9].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[8].championName,
-        name: matchInfo.data.info.participants[8].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[7].championName,
-        name: matchInfo.data.info.participants[7].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[6].championName,
-        name: matchInfo.data.info.participants[6].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[5].championName,
-        name: matchInfo.data.info.participants[5].summonerName,
-      },
-    ],
-    notmyTeam: [
-      {
-        champ: matchInfo.data.info.participants[0].championName,
-        name: matchInfo.data.info.participants[0].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[1].championName,
-        name: matchInfo.data.info.participants[1].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[2].championName,
-        name: matchInfo.data.info.participants[2].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[3].championName,
-        name: matchInfo.data.info.participants[3].summonerName,
-      },
-      {
-        champ: matchInfo.data.info.participants[4].championName,
-        name: matchInfo.data.info.participants[4].summonerName,
-      },
-    ],
+    gameType: matchInfo.data.info.gameMode, // 확실
+    gameResult: win, // 수정 완료 자기자신
+    champName: championName, // 수정완료
+    gameStat: stat,
+    myTeam: myteamlist,
+    notmyTeam: notmyteamlist,
   };
-  console.log(allInfo);
-  // console.log(typeof allInfo);
   return allInfo;
 };
