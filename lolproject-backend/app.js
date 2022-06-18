81d666c2c8ae222f5307e1c1a986f85e70e61751
jaewoo <comjayoncloud@gmail.com>
2022-06-18 23:54:37 +0900

    api token update

--- lolproject-backend/app.js
+++ lolproject-backend/app.js
@@ -45,7 +45,7 @@ getSummoner = async (name) => {
   // puuid 가져오기
   const summoner = await axios.get(encoded, {
     headers: {
-      "X-Riot-Token": "RGAPI-45ff146c-1ce7-41c0-9d70-8c1973e3cc0c",
+      "X-Riot-Token": "RGAPI-3582eb11-907d-4def-80f8-f569c618ae21",
     },
   });
   return summoner.data;
@@ -57,7 +57,7 @@ getMatchId = async (puuid) => {
     `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}`,
     {
       headers: {
-        "X-Riot-Token": "RGAPI-45ff146c-1ce7-41c0-9d70-8c1973e3cc0c",
+        "X-Riot-Token": "RGAPI-3582eb11-907d-4def-80f8-f569c618ae21",
       },
     }
   );
@@ -71,7 +71,7 @@ getMatch = async (s, summoner) => {
     `https://asia.api.riotgames.com/lol/match/v5/matches/${s}`,
     {
       headers: {
-        "X-Riot-Token": "RGAPI-45ff146c-1ce7-41c0-9d70-8c1973e3cc0c",
+        "X-Riot-Token": "RGAPI-3582eb11-907d-4def-80f8-f569c618ae21",
       },
     }
   );
